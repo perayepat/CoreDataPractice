5f4932e13477b288b1dd6805fcaeed0ee01e0978
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-22 08:32:24 +0200

    :notebook:  Normal Filtering not dynamic

--- CDataTut/ContentView.swift
+++ CDataTut/ContentView.swift
@@ -5,13 +5,59 @@
 //  Created by Pat on 2022/08/22.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    //Predicates
+    /// "universe IN %@", ["Aliens","Firefly","Star Trek"] -- gives you items from those arrays
+    /// "name BEGINSWITH %@", "E"  -- Everyhting with E which is case sensative
+    /// "name BEGINSWITH[c]  %@" , "e"  -- Case insensatve search
+    ///  "NOT name BEGINSWITH[c]  %@, "e""
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name < %@", "F")) var ships: FetchedResults<Ship>
     
     var body: some View {
+        VStack{
+            List(ships, id: \.self){ ship in
+                Text(ship.name ?? "UNK")
+            }
+            
+            Button("Add Examples"){
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//MARK: - Wizard with same name
+struct WizTest: View{
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    
+    var body: some View{
         VStack{
             List(wizards,id:\.self){ wizard in
                 Text(wizard.name ?? "UNK")
@@ -28,12 +74,6 @@ struct ContentView: View {
                 }
             }
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
 
